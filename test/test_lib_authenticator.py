f813132eaa879732643bb96a930faa9c5d8d0026
ytyaru <yttry0@gmail.com>
2021-08-13 12:35:22 +0900

    add test

---
+++ test/test_lib_authenticator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# coding: utf8
+import requests
+import os, sys, argparse, json, urllib.parse, datetime
+from string import Template
+from abc import ABCMeta, abstractmethod
+import mimetypes
+# 親ディレクトリをパスに追加する
+sys.path.append(os.path.join(os.path.dirname(__file__), '../src'))
+from lib import exept_null, Path, FileReader, FileWriter, Authenticator, Api
+import unittest
+from unittest.mock import MagicMock, patch, mock_open
+import copy
+class TestAuthenticator(unittest.TestCase):
+    def test_token(self):
+        test_data = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+        mock_io = mock_open(read_data=test_data)
+        with patch('builtins.open', mock_io):
+            actual = FileReader.text('/tmp/token.txt')
+            mock_io.assert_called_once_with('/tmp/token.txt', mode='r', encoding='utf-8')
+            self.assertEqual(actual, test_data)
+    @patch("builtins.open", MagicMock(side_effect=Exception()))
+    def test_token_except(self):
+        with self.assertRaises(Exception, msg='token.txtがありません。マストドンのインスタンスサーバでアカウントを作り、アプリを作って、アクセストークンを取得し、その値をtoken.txtに書いて保存してください。'):
+            Authenticator().Token
+
+if __name__ == "__main__":
+    unittest.main()
